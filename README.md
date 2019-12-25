b544416b9d05941ee193469fc40490539009b1b1
Sultan <smob123@hotmail.com>
2019-12-25 20:17:41 +1300

    fixed project structure

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Income-Inequality-Project
-This is a project for the course INFO 281, which aims to visualize income distribution inequality within New Zealand based on age groups, which are divided into people under 40 years old and ones who are over 40 years old.
+This is a project for the course INFO 281, which aims to visualize income distribution inequality within New Zealand between the Baby Boomer generation, and the younger generations, using a dataset of incomes from 2011. It also explores if a qualifications, or occupations have a noticeable effect on weekly incomes. As well as, checking if there is a certain age group that is noticeably disadvantaged by the wealth distribution.
+
+# Link to Application
+https://sultanbanabila.shinyapps.io/Project/
 
 # Technologies Used
 - R
 - Shiny
 
 # Screenshots
-<img src ="https://user-images.githubusercontent.com/26127333/71240168-52749180-236d-11ea-932c-c7a0cb5b30a1.PNG" />
-<img src ="https://user-images.githubusercontent.com/26127333/71240170-530d2800-236d-11ea-9a99-6a38470b9c60.PNG" />
-<img src ="https://user-images.githubusercontent.com/26127333/71240171-530d2800-236d-11ea-8977-b3fe747a1f78.PNG" />
-<img src ="https://user-images.githubusercontent.com/26127333/71240172-530d2800-236d-11ea-9ea7-6154e4a6f787.PNG" />
+<img src ="https://user-images.githubusercontent.com/26127333/71436621-27b28080-2753-11ea-8d2a-93f45a7c5a21.PNG" />
+<img src ="https://user-images.githubusercontent.com/26127333/71436622-27b28080-2753-11ea-98b5-e201a79b40b5.PNG" />
+<img src ="https://user-images.githubusercontent.com/26127333/71436623-27b28080-2753-11ea-9a8d-507366a28178.PNG" />
+<img src ="https://user-images.githubusercontent.com/26127333/71436624-284b1700-2753-11ea-81e9-6b555b96ce09.PNG" />
--- scripts/Project/app.R
+++ scripts/Project/app.R
@@ -19,19 +19,20 @@ library(ggplot2)
 incomes <-
     read.csv("../../data/translated_data.csv", stringsAsFactors = FALSE)
 
-# calculate incomes based on whether an indivudual is over or under 40 years old
-over.40 <- incomes[incomes$agegrp >= 40, ]
-under.40 <- incomes[incomes$agegrp < 40, ]
+# get the number of people in the dataset
+num.of.rows <- nrow(incomes)
 
-over.40.average <- round(mean(over.40[, "income"]), digits = 2)
-under.40.average <- round(mean(under.40[, "income"]), digits = 2)
+# get the number of people in each age group
+sample.sizes <- table(incomes$agegrp)
 
-# add the result to a dataframe to plot it
-younger.older.40.df <-
-    data.frame(
-        age_group = c("Over 40", "Under 40"),
-        income_averages = c(over.40.average, under.40.average)
-    )
+# fix column names to display age ranges properly
+names(sample.sizes) <-
+    paste0(names(sample.sizes), "-", as.numeric(names(sample.sizes)) + 4)
+names(sample.sizes)[11] <- "65+"
+
+# store the result in a dataframe, and name the columns properly
+sample.sizes.df <- as.data.frame(sample.sizes)
+colnames(sample.sizes.df) <- c("Age Group", "Sample Size")
 
 # calculate average incomes based on age groups
 get.income.by.age <- function() {
@@ -39,7 +40,7 @@ get.income.by.age <- function() {
     averages <- c()
     
     for (i in 1:length(ages)) {
-        x <- incomes[incomes$agegrp == ages[i], ]
+        x <- incomes[incomes$agegrp == ages[i],]
         averages[i] <- round(mean(x$income), digits = 2)
     }
     
@@ -48,11 +49,80 @@ get.income.by.age <- function() {
 
 averages <- get.income.by.age()
 
-# add the incomes by age into a dataframe to be plotted
+# add the income averages by age into a dataframe
 incomes.by.age.df <-
     data.frame(agegrp = c(15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65),
                income_averages = averages)
+# get the maximum average income, and the age group that is associated with it
+max.average.income <- max(incomes.by.age.df$income_averages)
+max.average.age <-
+    incomes.by.age.df[incomes.by.age.df$income_averages == max.average.income, "agegrp"]
+max.average.agegrp <-
+    paste0(max.average.age, "-", max.average.age + 4)
 
+
+# get the minimum average income, and the age group that is associated with it
+min.average.income <- min(incomes.by.age.df$income_averages)
+min.average.age <-
+    incomes.by.age.df[incomes.by.age.df$income_averages == min.average.income, "agegrp"]
+min.average.agegrp <-
+    paste0(min.average.age, "-", min.average.age + 4)
+
+
+# get the summary of the data to print it on the sidebar
+data.summary.df <-
+    data.frame(
+        "Age Group" = c(max.average.agegrp, min.average.agegrp),
+        "Average" = c(max.average.income, min.average.income)
+    )
+
+# calculate incomes based on whether an indivudual is over or under 40 years old
+over.40 <- incomes[incomes$agegrp >= 40,]
+under.40 <- incomes[incomes$agegrp < 40,]
+
+over.40.average <- round(mean(over.40[, "income"]), digits = 2)
+under.40.average <- round(mean(under.40[, "income"]), digits = 2)
+
+# add the result to a dataframe
+younger.older.40.df <-
+    data.frame(
+        age_group = c("Over 40", "Under 40"),
+        income_averages = c(over.40.average, under.40.average)
+    )
+
+# get the number of people who have an occupation for both age groups
+over.40.have.occupation <-
+    over.40[over.40$occupation != "No Occupation", ]
+
+under.40.have.occupation <-
+    under.40[under.40$occupation != "No Occupation", ]
+
+
+# get the list of people over 40 who earn at least $1 a week
+over.40.filter.one_dollar <-
+    over.40.have.occupation[over.40.have.occupation$income >= 1, ]
+
+# get the list of people under 40 who earn at least $1 a week
+under.40.filter.one_dollar <-
+    under.40.have.occupation[under.40.have.occupation$income >= 1, ]
+
+# get the list of the top 1% of 40+ year olds
+over.40.ordered <-
+    order(over.40.have.occupation$income, decreasing = TRUE)
+over.40.ordered <- over.40.have.occupation[over.40.ordered, ]
+
+one.percent <- ceiling(nrow(over.40.ordered) * 0.1)
+
+over.40.top_one_percent <- over.40.ordered[1:one.percent, ]
+
+# get the list of the top 1% of people under 40 years old
+under.40.ordered <-
+    order(under.40.have.occupation$income, decreasing = TRUE)
+under.40.ordered <- over.40.have.occupation[under.40.ordered, ]
+
+one.percent <- ceiling(nrow(under.40.ordered) * 0.1)
+
+under.40.top_one_percent <- under.40.ordered[1:one.percent, ]
 
 # calculates the frequencies of unique values in a dataframe's column
 calculate.frequencies <- function(dataset, var) {
@@ -64,66 +134,11 @@ calculate.frequencies <- function(dataset, var) {
     return(data.frame(counts))
 }
 
-# get the list of people over 40 who earn at least $1 a week
-over.40.filter.one_dollar <-
-    over.40[over.40$income >= 1,]
-
-# get the list of people under 40 who earn at least $1 a week
-under.40.filter.one_dollar <-
-    under.40[under.40$income >= 1,]
-
-# get the list of the top 1% of 40+ year olds
-over.40.ordered <- order(over.40)
-over.40.ordered <- over.40[over.40.ordered,]
-
-one.percent <- ceiling(nrow(over.40.ordered) * 0.1)
-
-over.40.ordered <- over.40.ordered[1:one.percent,]
-
-over.40.top_one_percent <-
-    over.40[over.40.ordered$income >= 1,]
-
-# get the list of the top 1% of people under 40 years old
-under.40.ordered <- order(under.40)
-under.40.ordered <- over.40[under.40.ordered,]
-
-one.percent <- ceiling(nrow(under.40.ordered) * 0.1)
-
-under.40.ordered <- under.40.ordered[1:one.percent,]
-
-under.40.top_one_percent <-
-    under.40[under.40.ordered$income >= 1,]
-
-# calculate qualification frequencies for people who are 40+ years old
-over.40.qualification.frequency <-
-    calculate.frequencies(over.40, "qualification")
-
-# calculate qualification frequencies for people who are under 40 years old
-under.40.qualification.frequency <-
-    calculate.frequencies(under.40, "qualification")
-
-
-# calculate the qualification frequencies for people who earn at least $1
-over.40.qualification.frequency.one_dollar <-
-    calculate.frequencies(over.40.filter.one_dollar, "qualification")
-
-under.40.qualification.frequency.one_dollar <-
-    calculate.frequencies(under.40.filter.one_dollar, "qualification")
-
-
-# calculate the common qualifications for the top 1% of 40+ year olds
-over.40.qualification.frequency.top_one_percent <-
-    calculate.frequencies(over.40.top_one_percent, "qualification")
-
-under.40.qualification.frequency.top_one_percent <-
-    calculate.frequencies(under.40.top_one_percent, "qualification")
-
 # calculate occupation frequencies
 over.40.occupation.frequencies <-
-    calculate.frequencies(over.40, "occupation")
-
+    calculate.frequencies(over.40.have.occupation, "occupation")
 under.40.occupation.frequencies <-
-    calculate.frequencies(under.40, "occupation")
+    calculate.frequencies(under.40.have.occupation, "occupation")
 
 
 # calculate occupation frequency for everyone who earns at least $1
@@ -140,51 +155,46 @@ over.40.occupation.frequency.top_one_percent <-
 under.40.occupation.frequency.top_one_percent <-
     calculate.frequencies(under.40.top_one_percent, "occupation")
 
+# calculate average incomes based on the top 1% of each age group
+get.top.one.percent.income.by.age <- function() {
+    ages <- c(15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65)
+    averages <- c()
+    
+    for (i in 1:length(ages)) {
+        age.group <- incomes[incomes$agegrp == ages[i],]
+        # get the list of the top 1% of 40+ year olds
+        age.group.ordered <-
+            order(age.group$income, decreasing = TRUE)
+        age.group.ordered <- age.group[age.group.ordered, ]
+        
+        one.percent <- ceiling(nrow(age.group.ordered) * 0.1)
+        
+        age.group.top_one_percent <-
+            age.group.ordered[1:one.percent, ]
+        averages[i] <-
+            round(mean(age.group.top_one_percent$income), digits = 2)
+    }
+    
+    return(averages)
+}
+top.one.percents <- get.top.one.percent.income.by.age()
 
-# get the maximum average income, and the age group that is associated with it
-max.average.income <- max(incomes.by.age.df$income_averages)
-max.average.age <-
-    incomes.by.age.df[incomes.by.age.df$income_averages == max.average.income, "agegrp"]
-max.average.agegrp <-
-    paste0(max.average.age, "-", max.average.age + 4)
+# add the income averages of top 1% by age into a dataframe
+incomes.by.age.top.one.percent.df <-
+    data.frame(agegrp = c(15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65),
+               income_averages = top.one.percents)
 
-
-# get the minimum average income, and the age group that is associated with it
-min.average.income <- min(incomes.by.age.df$income_averages)
-min.average.age <-
-    incomes.by.age.df[incomes.by.age.df$income_averages == min.average.income, "agegrp"]
-min.average.agegrp <-
-    paste0(min.average.age, "-", min.average.age + 4)
-
-# get the number of people in each age group
-sample.sizes <- table(incomes$agegrp)
-
-# fix column names to display age ranges properly
-names(sample.sizes) <-
-    paste0(names(sample.sizes), "-", as.numeric(names(sample.sizes)) + 4)
-names(sample.sizes)[11] <- "65+"
-
-# store the result in a dataframe, and name the columns properly
-sample.sizes.df <- as.data.frame(sample.sizes)
-colnames(sample.sizes.df) <- c("Age Group", "Sample Size")
-
-# get the summary of the data to print it on the sidebar
-data.summary.df <-
-    data.frame(
-        "Averages" = c(max.average.income, min.average.income),
-        "Age Group" = c(max.average.agegrp, min.average.agegrp)
-    )
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     # Application title
-    titlePanel("Income Distribution in New Zealand"),
+    titlePanel("Income Inequality Distribution in New Zealand"),
     sidebarLayout(
         # side bar
         sidebarPanel(
             # summary of the dataset
             div(tags$b("Number of the People in the Dataset:"),
-                p(nrow(incomes))),
+                p(num.of.rows)),
             div(tags$b("Sample Sizes")),
             tableOutput("sample.sizes"),
             div(tags$b("Mean of all Income Averages:"),
@@ -206,39 +216,22 @@ ui <- fluidPage(
                 verbatimTextOutput("younger.older.40.info")
             ),
             tabPanel(
-                "Qualification Frequencies",
-                # display data filters in a row
-                fluidRow(# the first column contains the age group filter
-                    column(
-                        5,
-                        radioButtons(
-                            "qualifications.age.groups.filter",
-                            label = "age.group",
-                            choices = c("40+", "under40"),
-                            selected = "40+"
-                        )
+                "Occupations",
+                # frequency of hours worked per week
+                h3("Wrok Hours Per Week"),
+                selectInput(
+                    "hours.per.week.filter",
+                    "Filter:",
+                    c(
+                        "none" = "all",
+                        "Earn at least $1" = "$1 or more",
+                        "Top 1%" = "top 1%"
                     ),
-                    # the second column contains income filter
-                    column(
-                        5,
-                        selectInput(
-                            "common.qualifications.filter",
-                            "Filter:",
-                            c(
-                                "none" = "all",
-                                "Earn at least $1" = "$1 or more",
-                                "Top 1%" = "top 1%"
-                            ),
-                            selected = "all"
-                        )
-                    )),
-                # print a summary of the data
-                plotOutput("qualification.frequency"),
-                verbatimTextOutput("qualification.frequency.info")
-            ),
-            tabPanel(
-                "Occupation Frequencies",
-                # display data filters in a row
+                    selected = "all"
+                ),
+                plotOutput("hours.per.week"),
+                # frequencies of various occupations
+                h3("Most Common Occupations"),
                 fluidRow(# the first column contains the age group filter
                     column(
                         5,
@@ -263,12 +256,12 @@ ui <- fluidPage(
                             selected = "all"
                         )
                     )),
-                # print a summary of the data
                 plotOutput("occupation.frequencies"),
                 verbatimTextOutput("occupation.frequencies.info")
             ),
+            # plot of age group specific income averages
             tabPanel(
-                "All Average Incomes",
+                "Average Incomes of the Top 1%",
                 plotOutput("incomes.by.age", click = "incomes.by.age.click"),
                 verbatimTextOutput("incomes.by.age.info")
             )
@@ -288,8 +281,48 @@ server <- function(input, output) {
         return(data.summary.df)
     })
     
+    # plot the average number of hours per week
+    output$hours.per.week <- renderPlot({
+        # get the chosen filter
+        plot.filter <- input$hours.per.week.filter
+        
+        # filter the data based on the chosen filter
+        if (plot.filter == "all") {
+            hours.per.week <- data.frame(
+                "age.group" = c("40+", "Under 40"),
+                "hours" = c(
+                    mean(over.40.have.occupation$hours),
+                    mean(under.40.have.occupation$hours)
+                )
+            )
+        } else if (plot.filter == "$1 or more") {
+            hours.per.week <- data.frame(
+                "age.group" = c("40+", "Under 40"),
+                "hours" = c(
+                    mean(over.40.filter.one_dollar$hours),
+                    mean(under.40.filter.one_dollar$hours)
+                )
+            )
+        } else if (plot.filter == "top 1%") {
+            hours.per.week <- data.frame(
+                "age.group" = c("40+", "Under 40"),
+                "hours" = c(
+                    mean(over.40.top_one_percent$hours),
+                    mean(under.40.top_one_percent$hours)
+                )
+            )
+        }
+        
+        ggplot(hours.per.week) +
+            geom_bar(aes(
+                x = age.group,
+                y = hours,
+                fill = age.group
+            ), stat = "identity")
+    })
+    
+    # compare average incomes for people who are older, or younger than 40 years old
     output$younger.older.40 <- renderPlot({
-        # plot people who are older, or younger than 40 years old
         ggplot(younger.older.40.df,
                aes(
                    x = "",
@@ -297,75 +330,15 @@ server <- function(input, output) {
                    fill = age_group
                )) +
             geom_bar(stat = "identity") +
-            ylab("Sum of Incomes for Each Age Group") +
+            ylab("Average of Incomes for Each Age Group") +
             coord_polar("y", start = 0)
     })
     
+    # print a summary of the plot above
     output$younger.older.40.info <- renderPrint({
         print(younger.older.40.df)
     })
     
-    # plot qualification frequencies for people who are 40 and above
-    output$qualification.frequency <- renderPlot({
-        agegrp.filter <- input$qualifications.age.groups.filter
-        plot.filter <- input$common.qualifications.filter
-        
-        if (plot.filter == "all") {
-            if (agegrp.filter == "40+") {
-                mcqPlot <- over.40.qualification.frequency
-            } else if (agegrp.filter == "under40") {
-                mcqPlot <- under.40.qualification.frequency
-            }
-        } else if (plot.filter == "$1 or more") {
-            if (agegrp.filter == "40+") {
-                mcqPlot <- over.40.qualification.frequency.one_dollar
-            } else {
-                mcqPlot <- under.40.qualification.frequency.one_dollar
-            }
-        } else if (plot.filter == "top 1%") {
-            if (agegrp.filter == "40+") {
-                mcqPlot <- over.40.qualification.frequency.top_one_percent
-            } else {
-                mcqPlot <- under.40.qualification.frequency.top_one_percent
-            }
-        }
-        
-        ggplot(mcqPlot) +
-            geom_bar(aes(
-                x = qualification,
-                y = Freq,
-                fill = qualification
-            ),
-            stat = "identity")
-    })
-    
-    # print the information of the dataframe
-    output$qualification.frequency.info <- renderPrint({
-        agegrp.filter <- input$qualifications.age.groups.filter
-        plot.filter <- input$common.qualifications.filter
-        
-        if (plot.filter == "all") {
-            if (agegrp.filter == "40+") {
-                mcqPlot <- over.40.qualification.frequency
-            } else if (agegrp.filter == "under40") {
-                mcqPlot <- under.40.qualification.frequency
-            }
-        } else if (plot.filter == "$1 or more") {
-            if (agegrp.filter == "40+") {
-                mcqPlot <- over.40.qualification.frequency.one_dollar
-            } else {
-                mcqPlot <- under.40.qualification.frequency.one_dollar
-            }
-        } else if (plot.filter == "top 1%") {
-            if (agegrp.filter == "40+") {
-                mcqPlot <- over.40.qualification.frequency.top_one_percent
-            } else {
-                mcqPlot <- under.40.qualification.frequency.top_one_percent
-            }
-        }
-        
-        print(mcqPlot)
-    })
     
     # plot occupation frequencies for people who are 40 and above
     output$occupation.frequencies <- renderPlot({
@@ -431,7 +404,7 @@ server <- function(input, output) {
     
     # plot average incomes of all age groups
     output$incomes.by.age <- renderPlot({
-        ggplot(incomes.by.age.df) +
+        ggplot(incomes.by.age.top.one.percent.df) +
             geom_point(aes(x = agegrp, y = income_averages), size = 3) +
             xlab("Age groups") +
             ylab("Sum of Incomes")
@@ -449,7 +422,7 @@ server <- function(input, output) {
             
             # get the data of the age group
             rowData <-
-                incomes.by.age.df[incomes.by.age.df$agegrp == rowNum,]
+                incomes.by.age.top.one.percent.df[incomes.by.age.top.one.percent.df$agegrp == rowNum, ]
             
             # check if there is a value in rowData
             if (length(rowData$agegrp) < 1) {
@@ -472,7 +445,7 @@ server <- function(input, output) {
                 
                 # get the list of incomes related to the
                 # selected age group from the original set
-                age.group <- incomes[incomes$agegrp == rowNum, ]
+                age.group <- incomes[incomes$agegrp == rowNum,]
                 
                 # print all the previus variabkes in seperate lines
                 cat(paste0(agegrp,
